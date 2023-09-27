3a80bf934a4e932b27ea992e24010774f4748f3a
sandesh <lokhandesandesh1998@gmail.com>
2023-09-27 13:48:49 +0530

    For Loop Example

---
+++ 15_forloop1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in 1 2 3 4 5 6 7 8 9 10
+do
+    echo "The number is $i"
+done
+
+for name in Amar Akbar Anthony
+do
+    echo "The name is $name"
+done
---
+++ 16_for_with_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#getting values from a file "02_comment.sh"
+FILE="/home/sandesh/Myscripts/02_comment.sh"
+for item in $(cat $FILE)
+do
+  echo $item
+done
