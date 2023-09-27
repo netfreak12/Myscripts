bf7f4bf7ffd4fe8ab090e469623d7d4d5bcfc4d6
sandesh <lokhandesandesh1998@gmail.com>
2023-09-27 16:48:28 +0530

    Function demo

---
+++ 24_function_demo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# To make function
+function welcomeNote() {
+    echo "-------------------"
+    echo "Welcome!"
+    echo "-------------------"
+}
+
+# To call our function
+welcomeNote
+welcomeNote
+welcomeNote
